823dd824568b6d063870aa211176f6357bd66b3c
lightballer <bokiiartem@gmail.com>
2021-09-27 20:18:57 +0300

    add player tests, removed on null tests in wall tests

--- core/Quoridor.Core.Test/Models/WallTests.cs
+++ core/Quoridor.Core.Test/Models/WallTests.cs
@@ -15,11 +15,6 @@ namespace Quoridor.Core.Test
             Assert.NotNull(wall);
         }
         [Test]
-        public void Constructor_Null_Arguments_Test()
-        {
-            Assert.Throws<ArgumentNullException>(() => new Wall(null, null));
-        }
-        [Test]
         public void Getter_Test()
         {
             Point[] start = { new Point(1, 1), new Point(1, 2) };
@@ -33,15 +28,6 @@ namespace Quoridor.Core.Test
             Assert.AreEqual(wall.End[0].Y, end[0].Y);
             Assert.AreEqual(wall.End[1].X, end[1].X);
             Assert.AreEqual(wall.End[1].Y, end[1].Y);
-        }
-        [Test]
-        public void Change_Point_Field_By_Get_Object_Test()
-        {
-            Point[] start = { new Point(1, 1), new Point(1, 2) };
-            Point[] end = { new Point(2, 1), new Point(2, 2) };
-            Wall wall = new Wall(start, end);
-            wall.Start[0].X = 5;
-            Assert.AreNotEqual(wall.Start[0].X, 5);
         }
     }
 }
