fd14736f801966fef8f1104ea7cd0de9e2e0905a
lightballer <bokiiartem@gmail.com>
2021-09-27 20:44:19 +0300

    revert state

--- core/Quoridor.Core/Exceptions/PlayerLimitReachedException.cs
+++ core/Quoridor.Core/Exceptions/PlayerLimitReachedException.cs
@@ -5,7 +5,6 @@ namespace Quoridor.Core.Exceptions
     class PlayerLimitReachedException : ArgumentException
     {
         private const string MESSAGE = "Cannot add more players! Player limit reached: ";
-
         public PlayerLimitReachedException(int limit) : base(MESSAGE + limit) { }
     }
 }
--- core/Quoridor.Core/Exceptions/PlayerNotFoundException.cs
+++ core/Quoridor.Core/Exceptions/PlayerNotFoundException.cs
@@ -5,7 +5,6 @@ namespace Quoridor.Core.Exceptions
     class PlayerNotFoundException : NullReferenceException
     {
         private const string MESSAGE = "Cannot find player with such id: ";
-
         public PlayerNotFoundException(int id) : base(MESSAGE + id) { }
     }
 }
