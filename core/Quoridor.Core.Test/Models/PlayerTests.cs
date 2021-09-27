9030d05f25ed9375ec139b7ea515b044d0df0368
lightballer <bokiiartem@gmail.com>
2021-09-27 19:07:47 +0300

    add wall tests

--- core/Quoridor.Core.Test/Models/PlayerTests.cs
+++ core/Quoridor.Core.Test/Models/PlayerTests.cs
@@ -5,8 +5,6 @@ namespace Quoridor.Core.Test
 {
     public class PlayerTests
     {
-        private GameEngine gameEngine;
-
         [Test]
         public void StubTest() // TODO: should be removed
         {
--- core/Quoridor.Core.Test/Models/WallTests.cs
+++ core/Quoridor.Core.Test/Models/WallTests.cs
@@ -5,12 +5,43 @@ namespace Quoridor.Core.Test
 {
     public class WallTests
     {
-        private GameEngine gameEngine;
-
         [Test]
-        public void StubTest() // TODO: should be removed
+        public void Constructor_Not_Null_Test()
         {
-            
+            Point[] start = { new Point(1, 1), new Point(1, 2) };
+            Point[] end = { new Point(2, 1), new Point(2, 2) };
+            Wall wall = new Wall(start, end);
+            Assert.NotNull(wall);
+        }
+        [Test]
+        public void Constructor_Null_Arguments_Test()
+        {
+            Wall wall = new Wall(null, null);
+            Assert.NotNull(wall);
+        }
+        [Test]
+        public void Getter_Test()
+        {
+            Point[] start = { new Point(1, 1), new Point(1, 2) };
+            Point[] end = { new Point(2, 1), new Point(2, 2) };
+            Wall wall = new Wall(start, end);
+            Assert.AreEqual(wall.Start[0].X, start[0].X);
+            Assert.AreEqual(wall.Start[0].Y, start[0].Y);
+            Assert.AreEqual(wall.Start[1].X, start[1].X);
+            Assert.AreEqual(wall.Start[1].Y, start[1].Y);
+            Assert.AreEqual(wall.End[0].X, end[0].X);
+            Assert.AreEqual(wall.End[0].Y, end[0].Y);
+            Assert.AreEqual(wall.End[1].X, end[1].X);
+            Assert.AreEqual(wall.End[1].Y, end[1].Y);
+        }
+        [Test]
+        public void Change_Point_Field_By_Get_Object_Test()
+        {
+            Point[] start = { new Point(1, 1), new Point(1, 2) };
+            Point[] end = { new Point(2, 1), new Point(2, 2) };
+            Wall wall = new Wall(start, end);
+            wall.Start[0].X = 5;
+            Assert.AreNotEqual(wall.Start[0].X, 5);
         }
     }
 }
