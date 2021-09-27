07a03c46fc0ccacebd5e9ed050e064b3dced12d8
lightballer <bokiiartem@gmail.com>
2021-09-27 18:41:53 +0300

    add point tests

--- core/Quoridor.Core.Test/Models/PointTests.cs
+++ core/Quoridor.Core.Test/Models/PointTests.cs
@@ -8,9 +8,26 @@ namespace Quoridor.Core.Test
         private GameEngine gameEngine;
 
         [Test]
-        public void StubTest() // TODO: should be removed
+        public void Constructor_Not_Null_Test() // TODO: should be removed
         {
-
+            Point point = new Point(5, 4);
+            Assert.NotNull(point);
         }
+        [Test]
+        public void Getter_Test()
+        {
+            Point point = new Point(5, 4);
+            Assert.AreEqual(point.X, 5);
+            Assert.AreEqual(point.Y, 4);
+        }
+        [Test]
+        public void Settter_Test()
+        {
+            Point point = new Point(5, 4);
+            point.X = 1;
+            Assert.AreEqual(point.X, 1);
+            point.Y = 2;
+            Assert.AreEqual(point.Y, 2);
+        } 
     }
 }
