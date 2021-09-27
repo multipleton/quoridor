f8d947e4190f25f9313a94ccd5b7db6d8f10493f
Yehor Bublyk <yehor.bk@gmail.com>
2021-09-27 19:55:11 +0300

    typo fixed

--- core/Quoridor.Core/Exceptions/PlayerLimitReachedException.cs
+++ core/Quoridor.Core/Exceptions/PlayerLimitReachedException.cs
@@ -5,6 +5,7 @@ namespace Quoridor.Core.Exceptions
     class PlayerLimitReachedException : ArgumentException
     {
         private const string MESSAGE = "Cannot add more players! Player limit reached: ";
+
         public PlayerLimitReachedException(int limit) : base(MESSAGE + limit) { }
     }
 }
