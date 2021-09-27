128989f1d24c44c915d11331b9a8533b7095a7a8
Yehor Bublyk <yehor.bk@gmail.com>
2021-09-27 19:55:11 +0300

    typo fixed

--- core/Quoridor.Core/Exceptions/InvalidPlayersCountException.cs
+++ core/Quoridor.Core/Exceptions/InvalidPlayersCountException.cs
@@ -5,6 +5,7 @@ namespace Quoridor.Core.Exceptions
     class InvalidPlayersCountException : ArgumentException
     {
         private const string MESSAGE = "Invalid players count! Must be 2 or 4, but provided: ";
+
         public InvalidPlayersCountException(int count) : base(MESSAGE + count) { }
     }
 }
