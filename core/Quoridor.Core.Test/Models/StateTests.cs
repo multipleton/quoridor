c3037a0f58583d9fa06dbc1e3e8dcd58e52bf15b
lightballer <bokiiartem@gmail.com>
2021-09-27 18:30:52 +0300

    add player, point, state, wall tests stubs

---
+++ core/Quoridor.Core.Test/Models/StateTests.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+using Quoridor.Core.Models;
+
+namespace Quoridor.Core.Test
+{
+    public class StateTests
+    {
+        private GameEngine gameEngine;
+
+        [Test]
+        public void StubTest() // TODO: should be removed
+        {
+       
+        }
+    }
+}
