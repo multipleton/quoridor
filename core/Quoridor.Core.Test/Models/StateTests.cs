4a270505de8161c44616af14845ade31b6478f09
lightballer <bokiiartem@gmail.com>
2021-09-28 11:14:42 +0300

    add state constructor tests

--- core/Quoridor.Core.Test/Models/StateTests.cs
+++ core/Quoridor.Core.Test/Models/StateTests.cs
@@ -1,14 +1,19 @@
 using NUnit.Framework;
 using Quoridor.Core.Models;
+using System;
 
 namespace Quoridor.Core.Test
 {
     public class StateTests
     {
         [Test]
-        public void StubTest() // TODO: should be removed
+        public void Constructor_Test()
         {
-       
+            State state2 = new State(2);
+            Assert.NotNull(state2);
+            State state4 = new State(4);
+            Assert.NotNull(state4);
+            Assert.Throws<ArgumentException>(() => new State(5));
         }
     }
 }
