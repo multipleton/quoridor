8e2a44bef74a2b79edf5f67eec6f4feddea0a680
lightballer <bokiiartem@gmail.com>
2021-10-06 21:39:37 +0300

    add basic game engine structure

--- core/Quoridor.Core/GameEngine.cs
+++ core/Quoridor.Core/GameEngine.cs
@@ -1,10 +1,77 @@
-﻿namespace Quoridor.Core
+﻿using Quoridor.Core.Models;
+
+namespace Quoridor.Core
 {
     public class GameEngine
     {
-        public string Stub() // TODO: should be removed
+        private string currentPlayer;
+        private State state;
+        private int playersCount;
+
+        public GameEngine(int playersCount)
         {
-            return "GameEngine";
+            state = new State((short)playersCount);
+        }
+
+        public void ConnectPlayer()
+        {
+            if (playersCount != 4)
+            {
+                playersCount++;
+            }
+
+        }
+
+        public void StartGame()
+        {
+            if (playersCount == 2 || playersCount == 4)
+            {
+                state = new State((short)playersCount);
+                for (int i = 0; i < playersCount; i++)
+                {
+                    state.AddPlayer();
+                }
+            }
+        }
+
+        private void MovePlayer(Point point)
+        {
+
+        }
+
+        private void PlaceWall(Point[] start, Point[] end)
+        {
+
+        }
+
+        public void MakeMove(Point point)
+        {
+            MovePlayer(point);
+        }
+
+        public void MakeMove(Point[] start, Point[] end)
+        {
+            PlaceWall(start, end);
+        }
+
+        public Point[] GetAvailiblePlayerFields()
+        {
+            return new Point[] { };
+        }
+
+        public Point[,] GetAvailibleWallPositions()
+        {
+            return new Point[,] { };
+        }
+
+        public void Reset()
+        {
+
+        }
+
+        public void EndGame()
+        {
+
         }
     }
 }
