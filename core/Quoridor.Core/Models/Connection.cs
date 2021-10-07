cf76575199456d4f94f1b97762efbc6f482ba1c2
Yehor Bublyk <yehor.bk@gmail.com>
2021-10-07 23:35:48 +0300

    Connection module added

---
+++ core/Quoridor.Core/Models/Connection.cs
@@ -0,0 +1,32 @@
+﻿namespace Quoridor.Core.Models
+{
+    public abstract class Connection
+    {
+        private readonly string identifier;
+        private short playerId;
+
+        public string Identifier => identifier;
+        public short PlayerId { get => playerId; set => playerId = value; }
+
+        public Connection(string identifier)
+        {
+            this.identifier = identifier;
+        }
+
+        public abstract void OnConnected();
+
+        public abstract void OnNewConnection(Connection connection);
+
+        public abstract void OnStart(Connection current);
+
+        public abstract void OnUpdate(State state);
+
+        public abstract void OnWaitingForMove();
+
+        public abstract void OnInvalidMove();
+
+        public abstract void OnMove(Connection previous, Connection current);
+
+        public abstract void OnFinish(Connection winner);
+    }
+}
