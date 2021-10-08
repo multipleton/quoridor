a89db35b729e84a474f9d5496e88dc04fd20089e
lightballer <bokiiartem@gmail.com>
2021-10-08 23:45:08 +0300

    add dfs method, add constructor to path finder

--- core/Quoridor.Core/Logic/PathFinder.cs
+++ core/Quoridor.Core/Logic/PathFinder.cs
@@ -1,8 +1,25 @@
-﻿namespace Quoridor.Core.Logic
+﻿using Quoridor.Core.Models;
+using System.Collections.Generic;
+using System;
+
+namespace Quoridor.Core.Logic
 {
     public class PathFinder
     {
-        private static Node[,] transformFieldStateToGraph(int[,] fieldState)
+        private Node[,] nodes;
+        private int _x;
+        private int _y;
+        private int _finalY;
+        private int[,] marked;
+        public PathFinder(int[,] fieldState, Point playerPosition, int finalY)
+        {
+            nodes = transformFieldStateToGraph(fieldState);
+            _x = playerPosition.X;
+            _y = playerPosition.Y;
+            _finalY = finalY;
+            marked = new int[9, 9];
+        }
+        private Node[,] transformFieldStateToGraph(int[,] fieldState)
         {
             var nodes = new Node[9, 9];
             for (int i = 0; i < 17; i = i + 2)
@@ -52,6 +69,59 @@
                 }
             }
             return nodes;
+        }
+
+        public bool DFS(int x = 8, int y = 4)
+        {
+            List<Point> availableList = new List<Point>();
+
+            if (nodes[x, y].Top == 0)
+            {
+                availableList.Add(new Point((short)(x - 1), (short)y));
+            }
+            if (nodes[x, y].Bottom == 0)
+            {
+                availableList.Add(new Point((short)(x + 1), (short)y));
+            }
+            if (nodes[x, y].Left == 0)
+            {
+                availableList.Add(new Point((short)x, (short)(y - 1)));
+            }
+            if (nodes[x, y].Right == 0)
+            {
+                availableList.Add(new Point((short)x, (short)(y + 1)));
+            }
+
+            var available = availableList.ToArray();
+
+            for (int i = 0; i < available.Length; i++)
+            {
+                if (x == 0) Console.WriteLine("Win");
+                x = available[i].X;
+                y = available[i].Y;
+                Console.WriteLine(x + " " + y);
+
+                if (marked[x, y] == 1) continue;
+                marked[x, y] = 1;
+
+                if (x - 1 > 0 && nodes[x, y].Top == 0)
+                {
+                    DFS(x - 1, y);
+                }
+                if (x + 1 < 9 && nodes[x, y].Bottom == 0)
+                {
+                    DFS(x + 1, y);
+                }
+                if (y - 1 > 0 && nodes[x, y].Left == 0)
+                {
+                    DFS(x, y - 1);
+                }
+                if (y + 1 < 9 && nodes[x, y].Right == 0)
+                {
+                    DFS(x, y + 1);
+                }
+            }
+            return false;
         }
 
         private class Node
