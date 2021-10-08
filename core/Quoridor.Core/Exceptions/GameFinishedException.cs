b72a6bb4ed2e6c4874628c5a7a42f04c327b3d62
Yehor Bublyk <yehor.bk@gmail.com>
2021-10-08 23:04:21 +0300

    GameFinishedException added

---
+++ core/Quoridor.Core/Exceptions/GameFinishedException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Quoridor.Core.Exceptions
+{
+    public class GameFinishedException : InvalidOperationException
+    {
+        private const string MESSAGE = "Cannot perform operation, the game is finished!";
+
+        public GameFinishedException() : base(MESSAGE) { }
+    }
+}
